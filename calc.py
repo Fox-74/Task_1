ba678e71beebb43abe7a1a9b48d0eb7645433d14
Fox-74 <orion73@rambler.ru>
2021-03-24 21:08:54 +0500

    Рабочий вариант V1

--- calc.py
+++ calc.py
@@ -6,24 +6,11 @@
 #        break
 #    except ValueError:
 #        print("Вы ввели не число! Попробуйте снова:")
-#-----------------------------------------------------
-#try:
 #========================================================
 #Ввод данных:
 n = float(input('Введите число: '))
 s = [] #Место хранения выражения
 nn = 0
-#i = -1 #Индикатор количества действий
-#while True:
-#    Nom_1 = list(input('Введите мат. операцию (+, -, *, **, /, //, %) и число: '))
-#    i+=1
-#    if Nom_1[0] != '=':
-#        s.append(Nom_1)
-#    else:
-#        print(i)
-#        print(s) #Проверка ввода.
-#        print(s[0],s[1][0])
-#        break
 #_____________________________________________________
 try:
     while nn != '=':
@@ -32,7 +19,7 @@ try:
             s = nn.replace('+','')
             nn = float(s)
             while True:
-                nu = input('Введите операцию и число или = для вывода результата1: ')
+                nu = input('Введите операцию и число или = для вывода результата: ')
                 if nu.startswith('*'):
                     s = nu.replace('*', '')
                     nu = float(s)
@@ -41,13 +28,22 @@ try:
                     s = nu.replace('/', '')
                     nu = float(s)
                     nn /= nu
+                elif nu.startswith('+'):
+                    s = nu.replace('+', '')
+                    nu = float(s)
+                    n += nn
+                    n += nu
+                elif nu.startswith('-'):
+                    s = nu.replace('-', '')
+                    nu = float(s)
+                    n += nn
+                    n -= nu
                 elif nu == '=':
                     n += nn
                     print(f'Результат: {n}')
                     print(s)
                     nn = nu
                     break
- #           n += nn
         elif nn.startswith('-'):
             s = nn.replace('-','')
             nn = float(s)
@@ -61,13 +57,22 @@ try:
                     s = nu.replace('/', '')
                     nu = float(s)
                     nn /= nu
+                elif nu.startswith('+'):
+                    s = nu.replace('+', '')
+                    nu = float(s)
+                    n += nn
+                    n += nu
+                elif nu.startswith('-'):
+                    s = nu.replace('-', '')
+                    nu = float(s)
+                    n += nn
+                    n -= nu
                 elif nu == '=':
                     n -= nn
                     print(f'Результат: {n}')
                     print(s)
                     nn = nu
                     break
-#            n -= nn
         elif nn.startswith('*'):
             s = nn.replace('*','')
             nn = float(s)
@@ -76,21 +81,10 @@ try:
             s = nn.replace('/', '')
             nn = float(s)
             n /= nn
- #       elif '**' in nn == True:
- #           n = list(nn)
- #           n **= float(nn[1])
         elif nn == '=':
             print(f'Результат: {n}')
             print(s)
             break
-#       elif (nn[0]=='/' or nn[0]=='//' or nn[0]=='%') and nn[1]=='0':
-#          print ('Ошибка! На ноль делить нельзя!')
-#        elif nn[0]=='/':
-#            n1/=float(nn[1])
-#        elif nn[0]=='//':
-#            n1//=float(nn[1])
-#        elif nn[0]=='%':
-#            n1%=float(nn[1])
         else:
             print ('Ошибка ввода!')
 except ValueError:
